75682003e536ea8ea0076e118e0a264ee82099e0
LBT <fnight644@gmail.com>
2024-06-07 20:11:59 +0700

    ErrorCode to handle exception

--- src/main/java/com/example/food/enums/ErrorCode.java
+++ src/main/java/com/example/food/enums/ErrorCode.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatusCode;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public enum ErrorCode {
-    USER_NOT_FOUND(1000, "User not found!", HttpStatus.NOT_FOUND),
+    USER_NOT_FOUND(1001, "User not found!", HttpStatus.NOT_FOUND),
     ;
 
     int code;
